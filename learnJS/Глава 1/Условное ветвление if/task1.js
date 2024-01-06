5254a7a8e8dba1634f8c1a91dd65b48b4bb49002
Roman07BY <markovich01@yandex.ru>
2024-01-06 13:31:18 +0300

    24

--- learnJS/Глава 1/Условное ветвление if/task1.js
+++ learnJS/Глава 1/Условное ветвление if/task1.js
@@ -1,8 +1,8 @@
 alert('Какое «официальное» название JavaScript?');
 let company = prompt('');
 if (company === 'ECMAScript') {
-  alert('Верно!');
+	alert('Верно!');
 } else {
-  alert('Не знаете? ECMAScript!');
+	alert('Не знаете? ECMAScript!');
 }
 
