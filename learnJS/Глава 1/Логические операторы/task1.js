107cce7c27f7216906d77aedf26823ef7df2e1ea
Roman Markovich <markovich01@yandex.ru>
2024-01-07 17:14:45 +0300

    235

---
+++ learnJS/Глава 1/Логические операторы/task1.js
@@ -0,0 +1,10 @@
+let age = prompt();
+
+if (age >= 14 && age <= 90) 
+{
+  alert("вх в диап");
+} 
+else 
+{
+  alert("не вх в диап");
+}
---
+++ learnJS/Глава 1/Логические операторы/task2.js
@@ -0,0 +1,21 @@
+let age=prompt();
+
+if(!(age>=14 && age<=90))
+{
+  alert('не вх в диап');
+}
+else
+{
+  alert('вх в диап');
+}
+
+
+// let age=prompt();
+
+// if(age<14 || age>90)
+// {
+//   alert('не вх в диап');
+// }
+// else{
+//   alert('вх в диап');
+// }
