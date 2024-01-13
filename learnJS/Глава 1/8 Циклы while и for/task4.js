340f098fe2feb916a1d6f4685bf8224d9a0e4d14
Roman Markovich <markovich01@yandex.ru>
2024-01-13 20:50:21 +0300

    code2

--- learnJS/Глава 1/8 Циклы while и for/task4.js
+++ learnJS/Глава 1/8 Циклы while и for/task4.js
@@ -2,7 +2,7 @@ let sum = 0;
 const ArrayOfNumbers = [1,2,-3,-4,-5,-6,-7,8,9,-10];
 for(let index = 0; index < ArrayOfNumbers.length; index++)
 {
-    if(ArrayOfNumbers[index] >0)
+    if(ArrayOfNumbers[index] %2==0 && ArrayOfNumbers[index]>0)
     {
         sum += ArrayOfNumbers[index];
     }  
