e6fa21e1b156e7d9cae1cd926106d1664da238ca
Roman07BY <markovich01@yandex.ru>
2024-01-13 18:53:08 +0300

    212

---
+++ learnJS/Глава 1/8 Циклы while и for/task4.js
@@ -0,0 +1,17 @@
+const arrayOfNumbers = [1,2,3,4,5,6,7,8,9,10];
+let i=0;
+//arrayOfNumbers = i;
+arrayOfNumbers[i];
+console.log(`${i}`);
+
+
+
+
+
+//for(i)
+
+
+
+//function positiveOfNumbers(arrayOfNumbers){
+//	for(let index=0; index<=arrayOfNumbers.length; )
+//}
