86fe8ad45726506443d5b46d3afa8585907c9ce0
Roman07BY <markovich01@yandex.ru>
2024-01-14 21:32:22 +0300

    pyaterkaaaaaaaaa

--- learnJS/Глава 1/8 Циклы while и for/task4.js
+++ learnJS/Глава 1/8 Циклы while и for/task4.js
@@ -3,7 +3,8 @@
 let sum = 0;
 const ArrayOfNumbers = [1, 2, -3, -4, -5, -6, -7, 8, 9, -10];
 for (let index = 0; index < ArrayOfNumbers.length; index++) {
-  if (ArrayOfNumbers[index] % 2 == 0 && ArrayOfNumbers[index] > 0) {
+  if (ArrayOfNumbers[index] % 2 == 0 && ArrayOfNumbers[index] > 0) 
+  {
     sum += ArrayOfNumbers[index];
   }
 }
@@ -15,7 +16,8 @@ const ArrayOfNumberss = [1, 2, -3, -4, -5, -6, -7, 8, 9, -10];
 const sumOfPositiveNumbers = (ArrayOfNumbers) => {
   let sum = 0;
   for (let index = 0; index < ArrayOfNumberss.length; index++) {
-    if (ArrayOfNumberss[index] % 2 == 0 && ArrayOfNumberss[index] > 0) {
+    if (ArrayOfNumberss[index] % 2 == 0 && ArrayOfNumberss[index] > 0) 
+    {
       sum += ArrayOfNumberss[index];
     }
   }
--- learnJS/Глава 1/8 Циклы while и for/task6.js
+++ learnJS/Глава 1/8 Циклы while и for/task6.js
@@ -1,14 +1,26 @@
 //задача на поиск элемента в массиве, если данный элемент присутствует вывод индекс номера
-const array = ["apple", "room", "orange", "needle", "tea", "carrot"];
+const arrayStrings = ["apple", "room", "orange", "needle", "tea", "carrot"];
 
-const findNeedle = (array, target) => {
-	for (let index = 0; index < array.length; index++) {
-		const element = array[index];
-		if(element === target){
-			return index;
-		}
-	}
+//const findNeedle = (array, target) => {
+//	for (let index = 0; index < array.length; index++) {
+//		const element = array[index];
+//		if(element === target){
+//			return index;
+//		}
+//	}
+//}
+//console.log('found the needle at position', `${findNeedle(array, "needle")}`);
+
+function findNeedle(array) {
+  for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    if (element === "needle") {
+      return `found the needle at position ${index}`;
+    }
+  }
 }
-console.log('found the needle at position', `${findNeedle(array, "needle")}`);
+const resultOfFN = findNeedle(arrayStrings); //вызов функции
+console.log(resultOfFN);
+
 
 
