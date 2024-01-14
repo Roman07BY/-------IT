8573e7efe9b3229b8b968c38148994efdad78df9
Roman07BY <markovich01@yandex.ru>
2024-01-14 20:34:27 +0300

    123

--- learnJS/Глава 1/8 Циклы while и for/task4.js
+++ learnJS/Глава 1/8 Циклы while и for/task4.js
@@ -1,3 +1,5 @@
+//задача на поиск четных положительных элементов в массиве и нахождение ех суммы
+//решение через циклы
 let sum = 0;
 const ArrayOfNumbers = [1, 2, -3, -4, -5, -6, -7, 8, 9, -10];
 for (let index = 0; index < ArrayOfNumbers.length; index++) {
@@ -7,4 +9,17 @@ for (let index = 0; index < ArrayOfNumbers.length; index++) {
 }
 console.log(`${sum}`);
 
-//задача на поиск четных положительных элементов в массиве и нахождение ех суммы
+
+//решение через функцию
+const ArrayOfNumberss = [1, 2, -3, -4, -5, -6, -7, 8, 9, -10];
+const sumOfPositiveNumbers = (ArrayOfNumbers) => {
+  let sum = 0;
+  for (let index = 0; index < ArrayOfNumberss.length; index++) {
+    if (ArrayOfNumberss[index] % 2 == 0 && ArrayOfNumberss[index] > 0) {
+      sum += ArrayOfNumberss[index];
+    }
+  }
+  return sum;
+}
+const resultOfFN = sumOfPositiveNumbers(ArrayOfNumberss);      //вызов функции
+console.log(resultOfFN);
--- learnJS/Глава 1/8 Циклы while и for/task6.js
+++ learnJS/Глава 1/8 Циклы while и for/task6.js
@@ -1,14 +1,14 @@
-//let sum = 0;
-const ArrayOfNumbers = ["apple", "room", "orange", "badroom", "tea", "carrot"];
-//for(let index = 0; index < ArrayOfNumbers.length; index++)
-//{
-//    if( ArrayOfNumbers[index]>0)
-//    {
-//        sum += ArrayOfNumbers[index]**2;
-//    }
-//}
-//console.log(`${sum}`);
-console.log(ArrayOfNumbers.includes("tea"));
-console.log(ArrayOfNumbers.indexOf("tea"));
+//задача на поиск элемента в массиве, если данный элемент присутствует вывод индекс номера
+const array = ["apple", "room", "orange", "needle", "tea", "carrot"];
 
-//задача на поиск элемента в массиве, если данный элемент присутствует вывод true/false соответственно и вывод индекс номера
+const findNeedle = (array, target) => {
+	for (let index = 0; index < array.length; index++) {
+		const element = array[index];
+		if(element === target){
+			return index;
+		}
+	}
+}
+console.log('found the needle at position', `${findNeedle(array, "needle")}`);
+
+
