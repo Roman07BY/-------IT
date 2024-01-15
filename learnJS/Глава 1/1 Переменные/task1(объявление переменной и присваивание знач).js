914ea3940a468af6147b94bff93d58443d70d2ff
Roman Markovich <markovich01@yandex.ru>
2024-01-15 10:16:01 +0300

    rename

--- learnJS/Глава 1/1 Переменные/task1(объявление переменной и присваивание знач).js
+++ learnJS/Глава 1/1 Переменные/task1(объявление переменной и присваивание знач).js
@@ -4,6 +4,6 @@
 //3)Скопируйте значение из переменной name в admin.
 //4)Выведите на экран значение admin, используя функцию alert (должна показать «Джон»).
 
-let admin, name ='Jhon';
-admin=name;
+let admin, name = 'Jhon';
+admin = name;
 console.log( `${admin}` );
--- learnJS/Глава 1/4 Базовые операторы, математика/task1.js
+++ learnJS/Глава 1/4 Базовые операторы, математика/task1.js
@@ -11,4 +11,8 @@ let a = 1, b = 1;
 let c = ++a; // 2
 let d = b++; // 1
 //a=2
-//b=2	
+//b=2
+console.log(`${a}`);
+console.log(`${b}`);
+console.log(`${c}`);
+console.log(`${d}`);	
