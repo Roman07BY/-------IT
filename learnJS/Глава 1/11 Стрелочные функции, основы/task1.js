9a2a1f4b979834c06aecb05e2039f03b20a37dd6
Roman Markovich <markovich01@yandex.ru>
2024-01-17 18:11:51 +0300

    2222

---
+++ learnJS/Глава 1/11 Стрелочные функции, основы/task1.js
@@ -0,0 +1,24 @@
+// Перепишите с использованием функции-стрелки
+
+// function ask(question, yes, no) {
+//   if (confirm(question)) yes()
+//   else no();
+// }
+
+// ask(
+//   "Вы согласны?",
+//   function() { alert("Вы согласились."); },
+//   function() { alert("Вы отменили выполнение."); }
+// );
+// _____________________________________________________________________________
+
+function ask(question, yes, no) {
+  if (confirm(question)) yes()
+  else no();
+}
+
+ask(
+  "Вы согласны?",
+  function() { alert("Вы согласились."); },
+  function() { alert("Вы отменили выполнение."); }
+);
---
+++ learnJS/Глава 1/11 Стрелочные функции, основы/Стрелочные функции, основы(теория).js
@@ -0,0 +1,10 @@
+let age = 15;
+let welcome = (age < 18) ? () => console.log('Привет!') : () => console.log("Здравствуйте!");
+welcome();
+
+let sum = (a, b) => {  // фигурная скобка, открывающая тело многострочной функции
+    let result = a + b;
+    return result; // если мы используем фигурные скобки, то нам нужно явно указать "return"
+  };
+  
+  console.log( sum(8, 2) ); // 3
