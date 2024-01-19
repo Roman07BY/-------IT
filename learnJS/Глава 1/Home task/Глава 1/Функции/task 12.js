f04c5664365c043699d65367ac99776d9922fc91
Roman07BY <markovich01@yandex.ru>
2024-01-19 17:11:42 +0300

    21

--- learnJS/Глава 1/Home task/Глава 1/Функции/task 12.js
+++ learnJS/Глава 1/Home task/Глава 1/Функции/task 12.js
@@ -1,11 +1,15 @@
 function noSpace(x) {
   let string = "";
   for (index = 0; index < x.length; index++) {
-	if(x[index]!== " "){
-	string += x[index] 
-}
+    if (x[index] !== " ") {
+      string += x[index];
+    }
   }
   return string;
 }
-noSpace("asdas asda asdad asda2 kfjf oowrj");
+
 console.log(noSpace("asdas asda asdad asda2 kfjf oowrj"));
+noSpace("asdas asda asdad asda2 kfjf oowrj");
+
+//console.log(basicOp("+", 9, 3));
+//basicOp = ("+", 9, 3);
--- learnJS/Глава 1/Home task/Глава 1/Функции/task 4.js
+++ learnJS/Глава 1/Home task/Глава 1/Функции/task 4.js
@@ -1,3 +1,11 @@
-//Write a function which calculates the average of the numbers in a given list.
-
-//Note: Empty arrays should return 0.
+function findAverage(array) {
+  let sumAverage = 0;
+  let sum = 0;
+  for (let index = 0; index < array.length; index++) {
+    sum += array[index];
+    sumAverage = sum / array.length;
+  }
+  return sumAverage;
+}
+console.log(findAverage([2, 4, 5, 7, 3, 23, 21, 11, 10]));
+findAverage = [2, 4, 5, 7, 3, 23, 21, 11, 10];
